762e71d3210b404f062ebf39666c2b38367cc400
dinesh_kuniyal <dineshkuniyal0@gmail.com>
2025-05-19 10:50:07 +0530

    Adds tests for edge case

--- tests/com/bootcamp/shapes/RectangleTest.java
+++ tests/com/bootcamp/shapes/RectangleTest.java
@@ -10,6 +10,22 @@ class RectangleTest {
         Rectangle rectangle = new Rectangle(2, 2);
         int area = rectangle.area();
 
-        assertEquals(4, area);
+        assertEquals(4, area, "should find the area of rectangle");
+    }
+
+    @Test
+    void areaWithNoLength() {
+        Rectangle rectangle = new Rectangle(0, 2);
+        int area = rectangle.area();
+
+        assertEquals(0, area, "should calculate the area with 0 length");
+    }
+
+    @Test
+    void areaWithNoBreadth() {
+        Rectangle rectangle = new Rectangle(2, 0);
+        int area = rectangle.area();
+
+        assertEquals(0, area, "should calculate the area with 0 breadth");
     }
 }
